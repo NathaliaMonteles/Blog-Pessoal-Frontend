61647fee72f25fec50f807ff0958f8392f70fea3
Nathalia Monteles <nathalia_martinsm@hotmail.com>
2024-06-22 23:44:41 -0300

    Iniciando o front com React

---
+++ src/paginas/home/Home.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Home.css';
+
+function Home(){
+    return (
+        <>
+            <h1 className="titulo">Home</h1>
+            <img src="https://wallpapers.com/images/hd/new-york-city-4k-ultra-hd-59x1sxl6wiyy4brl.jpg" alt="Imagem Tela Inicial" className="img" />
+        </>
+    );
+}
+
+export default Home;
