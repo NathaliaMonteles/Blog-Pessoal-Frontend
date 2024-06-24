ca6283b2bf68a3da82ac72f7bb499dbc25ae7c4b
Nathalia Monteles <nathalia_martinsm@hotmail.com>
2024-06-24 16:49:56 -0300

    hooks e props

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,36 @@
-import React from 'react';
+{/* } React, { useState } from 'react';
 import Home from './paginas/home/Home';
 import './App.css';
 
 
 function App() {
+  const [valor, setValor] = useState(0);
+
+  function handleClick() {
+    setValor(valor + 1);
+  }
   return (
-   <Home />
+   // <Home 
+   //title="Componente Home"
+   //description="Este é um componente Home que recebe props."
+   ///>
+   <div>
+    <p>O valor é: {valor}</p>
+    <button onClick={handleClick}>Adicionar 1</button>
+   </div>
+  );
+} */}
+
+import Home from './paginas/home/Home';
+import './App.css';
+import React from 'react';
+
+function App() {
+ 
+  return (
+    <>
+      <Home/>
+    </>
   );
 }
 
--- src/paginas/home/Home.tsx
+++ src/paginas/home/Home.tsx
@@ -1,13 +1,43 @@
-import React from 'react';
+{/*import React from 'react';
 import './Home.css';
 
-function Home(){
+interface minhaProps {
+    title: string;
+    description: string;
+}
+
+function Home(props:minhaProps){
     return (
         <>
-            <h1 className="titulo">Home</h1>
-            <img src="https://wallpapers.com/images/hd/new-york-city-4k-ultra-hd-59x1sxl6wiyy4brl.jpg" alt="Imagem Tela Inicial" className="img" />
+        {/* <h2>{props.title}</h2>
+        <p>{props.description}</p> */}
+        {/* <h1 className="titulo">Home</h1>
+        <img src="https://wallpapers.com/images/hd/new-york-city-4k-ultra-hd-59x1sxl6wiyy4brl.jpg" alt="Imagem Tela Inicial" className="img" />
+        </h1> 
         </>
     );
+} */}
+
+import React, { useState, useEffect } from 'react';
+
+function Home() {
+    const [completed, setCompleted] = useState(false);
+    const [tarefa, setTarefa] = useState('');
+
+    useEffect(() => {
+        if (completed) {
+            setTarefa('Parabéns! Você conluiu a tarefa');
+        }
+    }, [completed]);
+
+    return (
+        <div>
+            <h1>Tarefa</h1>
+            <h3>(tarefa)</h3>
+            <p>Conclua a tarefa</p>
+            <button onClick={() => setCompleted(true)}>Concluir Tarefa</button>
+        </div>
+    )
 }
 
 export default Home;
