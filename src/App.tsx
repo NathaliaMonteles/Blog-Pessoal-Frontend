08848c37c0ddef05452ca22a7fa5a3d7da5e2568
Nathalia Monteles <nathalia_martinsm@hotmail.com>
2024-06-26 15:58:31 -0300

    React Router Dom e Estilização

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,32 @@
-{/* } React, { useState } from 'react';
-import Home from './paginas/home/Home';
+/* import React from 'react';
 import './App.css';
-
+import Home from './paginas/home/Home';
 
 function App() {
-  const [valor, setValor] = useState(0);
-
-  function handleClick() {
-    setValor(valor + 1);
-  }
-  return (
-   // <Home 
-   //title="Componente Home"
-   //description="Este é um componente Home que recebe props."
-   ///>
-   <div>
-    <p>O valor é: {valor}</p>
-    <button onClick={handleClick}>Adicionar 1</button>
-   </div>
-  );
-} */}
-
-import Home from './paginas/home/Home';
-import './App.css';
-import React from 'react';
-
-function App() {
- 
   return (
     <>
-      <Home/>
+    <h1>Home</h1>
+    </>
+);
+}
+export default App; */
+
+import React from 'react';
+import './App.css';
+import Home from './paginas/home/home/Home';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Login from './paginas/home/login/Login';
+
+function App() {
+  return (
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/home" element={<Home />} />
+        </Routes>
+      </BrowserRouter>
     </>
   );
 }
