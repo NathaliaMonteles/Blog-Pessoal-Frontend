e1f2f84cb991f87c06cbb1eb96d5676911511f8e
Nathalia Monteles <nathalia_martinsm@hotmail.com>
2024-07-11 17:24:04 -0300

    Novo deploy banco de dados

--- src/paginas/home/Home.tsx
+++ src/paginas/home/Home.tsx
@@ -5,31 +5,30 @@ import ListaPostagens from '../../components/postagens/listaPostagens/ListaPosta
 import ModalPostagem from '../../components/postagens/modalPostagem/ModalPostagem';
 
 
-
 function Home() {
-    return (
-        <>
-        <div className="bg-pink-950 flex justify-center">
-          <div className='container grid grid-cols-2 text-white'>
-            <div className="flex flex-col gap-4 items-center justify-center py-4">
-              <h2 className='text-5xl font-bold'>Seja bem vinde!</h2>
-              <p className='text-xl'>Expresse aqui seus pensamentos e opniões</p>
-  
-              <div className="flex justify-around gap-4">
-              <ModalPostagem />
-              <button className='rounded bg-white text-pink-900 py-2 px-4'>Ver postagens</button>
-            </div>
-            </div>
-  
-            <div className="flex justify-center ">
-              <img src={homeLogo} alt="" className='w-2/3' />
-      
-            </div>
+  return (
+      <>
+      <div className="bg-pink-950 flex justify-center">
+        <div className='container grid grid-cols-2 text-white'>
+          <div className="flex flex-col gap-4 items-center justify-center py-4">
+            <h2 className='text-5xl font-bold'>Seja bem vinde!</h2>
+            <p className='text-xl'>Expresse aqui seus pensamentos e opniões</p>
+
+            <div className="flex justify-around gap-4">
+            <ModalPostagem />
+            <button className='rounded bg-white text-pink-900 py-2 px-4'>Ver postagens</button>
+          </div>
+          </div>
+
+          <div className="flex justify-center ">
+            <img src={homeLogo} alt="" className='w-2/3' />
+    
           </div>
         </div>
-        <ListaPostagens />
-      </>
-    );
+      </div>
+      <ListaPostagens />
+    </>
+  );
 }
 
 export default Home;
