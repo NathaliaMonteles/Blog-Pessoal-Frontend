0ed7e52fd089520c4bd020894e726f5309fbde4f
Nathalia Monteles <nathalia_martinsm@hotmail.com>
2024-07-12 10:36:33 -0300

    Ajuste footer

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -2,6 +2,8 @@ import { FacebookLogo, InstagramLogo, LinkedinLogo } from '@phosphor-icons/react
 import React, { useContext } from 'react'
 import { AuthContext } from '../../contexts/AuthContext'
 
+const linkedinLink = 'https://www.linkedin.com/in/nathalia-martins-monteles-a42117164/';
+
 function Footer() {
 
   const { usuario, handleLogout } = useContext(AuthContext)
@@ -18,7 +20,9 @@ function Footer() {
             <p className='text-xl font-bold'>Blog pessoal Generation | Copyright: {data}</p>
             <p className='text-lg'>Acesse nossas redes sociais</p>
             <div className='flex gap-2'>
-              <LinkedinLogo size={48} weight='bold' />
+            <a href={linkedinLink} target="_blank" rel="noopener noreferrer">
+              <LinkedinLogo size={30} weight='bold' />
+            </a>
               <InstagramLogo size={48} weight='bold' />
               <FacebookLogo size={48} weight='bold' />
             </div>
